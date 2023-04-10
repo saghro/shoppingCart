20ddea6c4fa34fa1715dd5cd7542f5c73ecc3225
HossameTor <hossametorchi07@gmail.com>
2023-04-10 14:24:43 +0000

    add animation

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import "./Cart.css"
+function Cart({ cartItems }) {
+  const totalPrice = cartItems.reduce((acc, item) => acc += item.price * item.quantity, 0);
 
-function Cart( {cartItems}) { 
   return (
     <div className="row my-4">
       <div className="col-md-">
@@ -17,47 +19,42 @@ function Cart( {cartItems}) {
                 <th></th>
               </thead>
               <tbody>
-                {
-                  cartItems.map(item =>(
-                    <tr key={item.id}>
-                      <td>{item.id}</td>
-                      <td> 
-                           <img src={item.image}
-                           className='fluid rounded'
-                           width={100}
-                           height={100}
-                           alt="" />
-                      </td>
-                      <td>
-                           {item.name}
-                      </td>
-                      <td>
-                        <i className="bi bi-caret-up"></i>
-                        <span className='mx-2'>
-                          {item.quantity}
-                        </span>
-                        <i className="bi bi-caret-down"></i>
-                      </td>
-                      <td>
-                        {item.price}$
-                      </td>
-                      <td> {item.price * item.quantity}</td>
-                      <td><i className="bi bi-caret-x text-danger"></i></td>
+                {cartItems.map(item => (
+                  <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>
+                      <img src={item.image}
+                        className='fluid rounded'
+                        width={100}
+                        height={100}
+                        alt="" />
+                    </td>
+                    <td>
+                      {item.name}
+                    </td>
+                    <td>
+                      <i className="bi bi-caret-up"></i>
+                      <span className='mx-2'>
+                        {item.quantity}
+                      </span>
+                      <i className="bi bi-caret-down"></i>
+                    </td>
+                    <td>
+                      {item.price}$
+                    </td>
+                    <td> {item.price * item.quantity}</td>
+                    <td><i className="bi bi-caret-x text-danger"></i></td>
 
-                    </tr>
-                  ))
-                }
+                  </tr>
+                ))}
                 <tr>
-                  <th colSpan={3}className='text-center'>
-                    Tolal
+                  <th colSpan={3} className='text-center'>
+                    Total
                   </th>
                   <td>
-                     <span className='badge bg-danger rounded-pill'>
-                       ${
-                        cartItems.reduce((acc,item) => acc += item.price * item.quantity,0)
-                       }
-
-                     </span>
+                    <span className='badge bg-danger rounded-pill'>
+                      {totalPrice.toFixed(2)}
+                    </span>
                   </td>
                 </tr>
 
@@ -66,9 +63,10 @@ function Cart( {cartItems}) {
           </div>
         </div>
       </div>
-      
+
     </div>
   );
 }
 
 export default Cart;
+
